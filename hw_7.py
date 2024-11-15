2386128e833b22a5dfbc7068de55b593ef392d1e
Valentin <nitnelav6691@gmail.cov>
2024-11-15 10:26:00 +0300

    Задача 2

--- hw_7.py
+++ hw_7.py
@@ -9,12 +9,33 @@
 # Ввод: Вывод:
 # пара-ра-рам рам-пам-папам па-ра-па-дам Парам пам-пам
 
-def ritm(text):
-    text1 = text.lower().split()
-    f = lambda x: sum(1 for i in x if i in 'аяуюоёиыеэ')
-    temp = f(text1[0])
-    if all([f(i) == temp for i in text1]):
-        return 'Парам пам-пам'
-    return 'Пам парам'
-text = 'пара-ра-рам рам-пам-папам па-ра-па-дам Парам-пам-пам'
-print(ritm(text))
+# def ritm(text):
+#     text1 = text.lower().split()
+#     f = lambda x: sum(1 for i in x if i in 'аяуюоёиыеэ')
+#     temp = f(text1[0])
+#     if all([f(i) == temp for i in text1]):
+#         return 'Парам пам-пам'
+#     return 'Пам парам'
+# text = 'пара-ра-рам рам-пам-папам па-ра-па-дам Парам-пам-пам'
+# print(ritm(text))
+
+# Задача 2: Напишите функцию print_operation_table(operation, num_rows=6, num_columns=6),
+# которая принимает в качестве аргумента функцию, вычисляющую элемент по номеру строки и
+# столбца. Аргументы num_rows и num_columns указывают число строк и столбцов таблицы,
+# которые должны быть распечатаны. Нумерация строк и столбцов идет с единицы (подумайте,
+# почему не с нуля). Примечание: бинарной операцией называется любая операция, у которой
+# ровно два аргумента, как, например, у операции умножения.
+# Ввод:                                         Вывод:
+# print_operation_table(lambda x, y: x * y)   1 2 3 4 5 6 
+#                                             2 4 6 8 10 12
+#                                             3 6 9 12 15 18
+#                                             4 8 12 16 20 24
+#                                             5 10 15 20 25 30
+#                                             6 12 18 24 30 36
+def print_operation_table(op, num_rows, num_columns):
+    for x in range(1, num_rows + 1):
+        list1 = list()
+        for y in range(1, num_columns + 1):
+            list1 += str(op(x, y)) + " "
+        print(*list1)
+print_operation_table(lambda x, y: x * y, 6, 6)
